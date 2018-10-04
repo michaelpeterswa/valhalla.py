6b3433340f4fa4b29242d76f2936c91245395c98
Michael Peters <michaelpeterswa@icloud.com>
2018-10-03 19:25:05 -0700

    Added valuable features: uptime, clear, and on message clear

--- valhalla.py
+++ valhalla.py
@@ -1,10 +1,12 @@
-import discord
+import discord, datetime, time
 from discord.ext import commands
 import random
 
-TOKEN = 'XXXXXXXXXXX'
+TOKEN = 'NDk2NzcxMDM4NzcyNzIzNzEy.DpVk0A.FKI8GwMz-fiLzNYcUgVfwXSOwQE'
 
 bot = commands.Bot(command_prefix='v ', description="valhalla")
+
+start_time = time.time()
 
 @bot.event
 async def on_ready():
@@ -34,7 +36,7 @@ async def roll(ctx, *, val):
 
 @bot.command(pass_context=True)
 async def kickornah(ctx):
-	"""decides whether to kick person or not"""
+	"""Decides whether to kick person or not"""
 	randNum = random.randint(0,1)
 	if randNum == 0:
 		embed = discord.Embed(title="The verdict is in:", description="Nah. You have been spared", color=0x00ff00)
@@ -42,5 +44,38 @@ async def kickornah(ctx):
 	if randNum == 1:
 		embed = discord.Embed(title="The verdict is in:", description="Kick. The decision is final", color=0xff0000)
 		await bot.send_message(ctx.message.channel, embed=embed)
+
+# @bot.event
+# async def on_message(message):
+# 	if not (message.author).bot:
+# 		if message.content.startswith('v '):
+# 			await bot.delete_message(message)
+# 	await bot.process_commands(message)
+
+@bot.command(pass_context=True)
+async def uptime(ctx):
+	"""Returns uptime"""
+	current_time = time.time()
+	difference = int(round(current_time - start_time))
+	text = str(datetime.timedelta(seconds=difference))
+	embed = discord.Embed(colour=ctx.message.author.top_role.colour)
+	embed.add_field(name="Uptime", value=text)
+	embed.set_footer(text="sponsored by nwradio#2779 | ran on RPi3")
+	try:
+		await bot.send_message(ctx.message.channel, embed=embed)
+	except discord.HTTPException:
+		await bot.say("Current uptime: " + text)
+
+
+def is_me(m):
+    return m.author == bot.user
+
+@bot.command(pass_context=True)
+async def clear(ctx):
+	"""Clears messages from bot"""
+	deleted = await bot.purge_from(ctx.message.channel, limit=100, check=is_me)
+	await bot.send_message(ctx.message.channel, 'Deleted {} message(s)'.format(len(deleted)))
+
+
 
 bot.run(TOKEN)
